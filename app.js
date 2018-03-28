16970d4cdfb68bcbad911d8120c371ad87eeeb97
metriccaution <xang@hotmail.co.uk>
2018-03-28 20:16:56 +0100

    Printing out a summary when saving a time

--- app.js
+++ app.js
@@ -25,58 +25,57 @@ const writeDb = (file, data) =>
 
 const args = () => process.argv.slice(2);
 
+const printSummary = () =>
+  readDb(config.db)
+    // Filter out bad data & simplify model
+    .then(intervals =>
+      intervals
+        .filter(i => i.start && i.stop && i.start < i.stop)
+        .map(i => ({ start: i.start, length: i.stop - i.start }))
+    )
+    // Group by day
+    .then(
+      groupBy(interval => Math.floor(interval.start / (1000 * 60 * 60 * 24)))
+    )
+    // Only get the top few days
+    .then(dayGroups =>
+      pick(
+        dayGroups,
+        Object.keys(dayGroups)
+          .sort((a, b) => b - a)
+          .slice(0, config.displayDays)
+      )
+    )
+    // Sum up each day
+    .then(dayGroups =>
+      values(dayGroups)
+        .map(day =>
+          day.reduce((summed, entry) => ({
+            start: summed.start,
+            length: summed.length + entry.length
+          }))
+        )
+        .map(day => [day.start, day.length])
+        .sort((pair1, pair2) => pair2[0] - pair1[0])
+    )
+    // Format each day
+    .then(days =>
+      days.map(day => {
+        const hoursWorked = (day[1] / (1000 * 60 * 60)).toFixed(1);
+        return moment(day[0]).format("DD/MM/YYYY") + " - " + hoursWorked;
+      })
+    )
+    // Print out each day
+    .then(days => days.forEach(day => console.log(day)));
+
 // If no arguments are passed, print out worked time
 if (args().length === 0) {
-  // Print result
-  return (
-    readDb(config.db)
-      // Filter out bad data & simplify model
-      .then(intervals =>
-        intervals
-          .filter(i => i.start && i.stop && i.start < i.stop)
-          .map(i => ({ start: i.start, length: i.stop - i.start }))
-      )
-      // Group by day
-      .then(
-        groupBy(interval => Math.floor(interval.start / (1000 * 60 * 60 * 24)))
-      )
-      // Only get the top few days
-      .then(dayGroups =>
-        pick(
-          dayGroups,
-          Object.keys(dayGroups)
-            .sort((a, b) => b - a)
-            .slice(0, config.displayDays)
-        )
-      )
-      // Sum up each day
-      .then(dayGroups =>
-        values(dayGroups)
-          .map(day =>
-            day.reduce((summed, entry) => ({
-              start: summed.start,
-              length: summed.length + entry.length
-            }))
-          )
-          .map(day => [day.start, day.length])
-          .sort((pair1, pair2) => pair2[0] - pair1[0])
-      )
-      // Format each day
-      .then(days =>
-        days.map(day => {
-          const hoursWorked = (day[1] / (1000 * 60 * 60)).toFixed(1);
-          return moment(day[0]).format("DD/MM/YYYY") + " - " + hoursWorked;
-        })
-      )
-      // Print out each day
-      .then(days => days.forEach(day => console.log(day)))
-  );
+  printSummary();
 }
 
 // Either parse the provided time, or just put out now
 const getTime = () => {
   const suppliedTime = args()[1];
-
   if (suppliedTime) {
     const parts = suppliedTime.split(":");
     const date = new Date();
@@ -106,17 +105,21 @@ const setStop = (intervals, stop) => {
   return intervals;
 };
 
+if (args().length === 0) return;
+
 // Update the db
 switch (args()[0].toLocaleLowerCase()) {
   case "start":
     return readDb(config.db)
       .then(d => setStart(d, getTime()))
       .then(d => writeDb(config.db, d))
+      .then(printSummary)
       .catch(e => console.log("Oh no!", e));
   case "stop":
     return readDb(config.db)
       .then(d => setStop(d, getTime()))
       .then(d => writeDb(config.db, d))
+      .then(printSummary)
       .catch(e => console.log("Oh no!", e));
   default:
     return console.log("I don't know what to do with '" + args()[0] + "'");
