9f529596abfeaa44dd0cce788fdd78f2742c6ce4
metriccaution <metriccaution@users.noreply.github.com>
2018-11-06 12:40:32 +0000

    Logging out when starting/stopping an interval

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,34 @@ import { handleError, help, start, stop, view } from "./commands";
 import config, { Commands } from "./config";
 import { FileDb, TimeDb } from "./db";
 
-const baseConfig = config.get();
-const { configFile, project } = baseConfig;
-if (existsSync(configFile) && lstatSync(configFile).isFile()) {
-  config.loadFile(configFile);
-}
+(async () => {
+  const baseConfig = config.get();
+  const { configFile, project } = baseConfig;
+  if (existsSync(configFile) && lstatSync(configFile).isFile()) {
+    config.loadFile(configFile);
+  }
 
-config.validate();
+  config.validate();
 
-const args = config.get();
-const db: TimeDb = new FileDb(args.file);
+  const args = config.get();
+  const db: TimeDb = new FileDb(args.file);
 
-switch (args.command) {
-  case Commands.Help:
-    help();
-    break;
-  case Commands.Start:
-    start(db, args.time, project).catch(handleError);
-    break;
-  case Commands.Stop:
-    stop(db, args.time, project).catch(handleError);
-    break;
-  case Commands.View:
-    view(db).catch(handleError);
-    break;
-  default:
-    throw new Error(`Unknown command "${args.command}"`);
-}
+  switch (args.command) {
+    case Commands.Help:
+      help();
+      break;
+    case Commands.Start:
+      await start(db, args.time, project);
+      await view(db);
+      break;
+    case Commands.Stop:
+      await stop(db, args.time, project);
+      await view(db);
+      break;
+    case Commands.View:
+      await view(db);
+      break;
+    default:
+      throw new Error(`Unknown command "${args.command}"`);
+  }
+})().catch(handleError);
